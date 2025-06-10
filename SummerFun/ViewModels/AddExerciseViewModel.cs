2e178328bd34061673a719552365f94b944edec7
claeoz <126366391+claeoz@users.noreply.github.com>
2025-06-10 09:19:31 -0500

    made ui layout for add exercise

--- SummerFun/ViewModels/AddExerciseViewModel.cs
+++ SummerFun/ViewModels/AddExerciseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using SummerFun.Enums;
 using SummerFun.Helper;
 using SummerFun.Models;
 
@@ -12,9 +13,27 @@ namespace SummerFun.ViewModels
 
 		public AddExerciseViewModel()
 		{
+			exercises = new HashSet<ExerciseModel>();
 			exercises = JSONHelper.LoadExercises();
 		}
-
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public List<Equipment> EquipmentOptions
+		{
+			get
+			{
+				return Enum.GetValues(typeof(Equipment)).Cast<Equipment>().ToList();
+			}
+		}
+		public List<Muscles> MusclesOptions
+		{
+			get
+			{
+				return Enum.GetValues(typeof(Muscles)).Cast<Muscles>().ToList();
+			}
+		}
+		public List<Muscles> MusclesUsed { get; set; }
+		public Equipment UsedEquipment { get; set; }
 		public ExerciseModel Exercise
 		{
 			get
