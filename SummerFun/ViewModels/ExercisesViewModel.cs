bb6a2263966f2f54d96206ff0879b7172117f49f
claeoz <126366391+claeoz@users.noreply.github.com>
2025-06-10 22:53:48 -0500

    Delete button functional

--- SummerFun/ViewModels/ExercisesViewModel.cs
+++ SummerFun/ViewModels/ExercisesViewModel.cs
@@ -14,13 +14,33 @@ namespace SummerFun.ViewModels
 		{
 			Exercises = new ObservableCollection<ExerciseModel>(JSONHelper.LoadExercises());
 			AddExerciseCommand = new Command(async () => await OnAddExercise());
+			EditCommand = new Command(async () => await OnEdit());
+			DeleteCommand = new Command(async () => await OnDelete());
 		}
 		private async Task OnAddExercise()
 		{
 			await Shell.Current.GoToAsync(nameof(AddExercisePage));
 		}
+		private async Task OnEdit()
+		{
+
+		}
+		private async Task OnDelete()
+		{
+			if (SelectedExercise != null)
+			{
+				Exercises.Remove(SelectedExercise);
+				JSONHelper.SaveExercises(Exercises.ToHashSet());
+			}
+			else
+			{
+				await Application.Current.MainPage.DisplayAlert("Nothing Selected", "Please select an object to delete.", "OK");
+			}
+		}
 		public ObservableCollection<ExerciseModel> Exercises { get; set; }
 		public ICommand AddExerciseCommand { get; set; }
+		public ICommand EditCommand { get; set; }
+		public ICommand DeleteCommand { get; set; }
 		public ExerciseModel SelectedExercise { get; set; }
 	}
 }
