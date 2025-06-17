0adbd18bc0681939b6419e9ba9316897bf51b5ed
claeoz <126366391+claeoz@users.noreply.github.com>
2025-06-17 00:19:14 -0500

    added community toolkit

--- SummerFun/MauiProgram.cs
+++ SummerFun/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace SummerFun
 {
@@ -10,6 +11,8 @@ namespace SummerFun
 			var builder = MauiApp.CreateBuilder();
 			builder
 				.UseMauiApp<App>()
+				.UseMauiCommunityToolkit()
+
 				.ConfigureFonts(fonts =>
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
